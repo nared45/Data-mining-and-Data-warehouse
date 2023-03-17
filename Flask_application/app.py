b70ad9d9c64b5926995d1f3dacd2b2ecf2a7ee0c
ChanakanSri_CPE <theghostarttv@gmail.com>
2023-03-17 13:45:06 +0700

    prepare flask

---
+++ Flask_application/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import pymysql
+
+
+app = Flask(__name__)
+app.secret_key = 'data_warehouse'
+
+# ==================================================================================
+# Connet database
+# def openConnection():
+#     connection = pymysql.connect(host='localhost',
+#                                  user='root',
+#                                  password='',
+#                                  database='itm_database',  # Name of database
+#                                  charset='utf8',
+#                                  #  cursorclass=pymysql.cursors.DictCursor
+#                                  )
+#     return connection
+# localhost, username, password, database
+# ==================================================================================
+if __name__ == "__main__":
+    app.run(debug=True
+    # , use_reloader=False
+    )
