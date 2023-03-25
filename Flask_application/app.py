47e68043b17e3e593744433369bff0bc4e5d6dc1
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-03-25 17:08:33 +0700

    add form with action in Flask

--- Flask_application/app.py
+++ Flask_application/app.py
@@ -30,27 +30,39 @@ def openConnection():
 # ============================= Route Web =============================
 # >> Not Login
 
-
 @app.route("/")
 def indexPage():
     return render_template('NotLogin/index.html')
-
 
 @app.route("/register")
 def registerPage():
     return render_template('NotLogin/register.html')
 
-
 @app.route("/login")
 def loginPage():
     return render_template('NotLogin/login.html')
 
+@app.route("/aboutUs")
+def aboutUsPage():
+    return render_template('NotLogin/aboutUs.html')
+
 # >> Login
+
+@app.route("/<string:id>/name/<string:name>")
+def loginFormPage(id, name):
+    if 'login' in session:
+        return render_template('Login/form.html', data_id = id, data_name = name)
+    else :
+        return redirect(url_for('indexPage'))
 
 # =====================================================================
 
 # ============================= Action =============================
 
+@app.route('/logout')
+def logout():
+    session.pop('login', None)
+    return redirect(url_for('indexPage'))
 
 @app.route("/register/actions", methods=['POST'])
 def registerAction():
@@ -79,6 +91,28 @@ def registerAction():
         else:
             flash("Password and Confirm Password Don't Match", "danger")
             return redirect(url_for('registerPage'))
+        
+@app.route("/login/actions", methods=['POST'])
+def loginAction():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        conn = openConnection()
+        cur = conn.cursor()
+        sql = "SELECT * FROM `employee` WHERE employee.email = %s"
+        cur.execute(sql, (email))
+        result = cur.fetchone()
+        conn.close()
+        if result == None :
+            flash("Don't have this User in database", "danger")
+            return redirect(url_for('loginPage'))
+        else :
+            if result and check_password_hash(result[2], password):
+                session['login'] = result[1]
+                return redirect(url_for('loginFormPage', id = result[0], name = result[3]))
+            else :
+                flash("Please check your password", "warning")
+                return redirect(url_for('loginPage'))
 
 
 # ==================================================================
